9dc38ba973204657dcb749ef172ec33a87692cbb
daflones <contatodaflonedu@gmail.com>
2025-10-07 14:10:29 -0300

    fix: corrigir caminho do catálogo PDF para /images/catalogo/INOVAPET.pdf

--- src/utils/downloadCatalog.ts
+++ src/utils/downloadCatalog.ts
@@ -1,6 +1,6 @@
 export const downloadCatalog = () => {
   const link = document.createElement('a');
-  link.href = '/catalogo/INOVAPET.pdf';
+  link.href = '/images/catalogo/INOVAPET.pdf';
   link.download = 'INOVAPET-Catalogo.pdf';
   link.target = '_blank';
   document.body.appendChild(link);
